a702a9002d67c6f7eb7ba606b19d22782fe8b501
mckinnley <mck.workman@gmail.com>
2020-05-03 10:47:17 -0400

    conf creation

---
+++ config.js
@@ -0,0 +1,21 @@
+config = {
+  "admin": {
+    "min": 1,
+    "max": 3
+  },
+  "providers_approved": {
+    "min": 0,
+    "max": 30
+  },
+  "providers_subscribed": {
+    "min": 0,
+    "max": 30
+  },
+  "providers_available": {
+    "min": 0,
+    "max": 30
+  },
+  "max_daily_serivce_requests": 3
+};
+
+module.exports = config;
